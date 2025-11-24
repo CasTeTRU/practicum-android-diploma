d9226390480fc6ddc7cb244d9358364d0a390eb5
forevershagal <07ramilshagaliev@gmail.com>
2025-11-24 18:13:17 +0300

    delete Resource.kt

--- app/src/main/java/ru/practicum/android/diploma/util/Resource.kt
+++
@@ -1,6 +0,0 @@
-package ru.practicum.android.diploma.util
-
-sealed class Resource<T>(val value: T? = null, val message: String? = null) {
-    class Success<T>(value: T) : Resource<T>(value)
-    class Error<T>(message: String, value: T? = null) : Resource<T>(value, message)
-}
