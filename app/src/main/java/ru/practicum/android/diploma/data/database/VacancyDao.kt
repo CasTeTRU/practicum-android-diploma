67673757f8e7351919b7c650b207b48a993dc846
CasTeTRU <kongalkin@gmail.com>
2025-11-25 23:04:50 +0300

    Добавил пустой vacancyDao чтобы detekt не ругался

--- app/src/main/java/ru/practicum/android/diploma/data/database/VacancyDao.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/database/VacancyDao.kt
@@ -3,6 +3,7 @@ package ru.practicum.android.diploma.data.database
 import androidx.room.Dao
 
 @Dao
-@Suppress("EmptyClassBlock")
+@Suppress("EmptyClassBlock", "NoEmptyClassBody")
 interface VacancyDao {
+    // Добавить методы для работы с избранными вакансиями
 }
