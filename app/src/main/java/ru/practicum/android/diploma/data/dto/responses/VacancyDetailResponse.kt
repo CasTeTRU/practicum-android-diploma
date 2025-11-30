44bd0fbcdd946e0ae3636fff6af270892f374c98
lizaKotsar <yelizaveta.kotsar@bk.ru>
2025-11-30 22:48:01 +0300

    Обновила VacancyDetailResponse.kt для эндпоинта /vacancies/{id}

--- app/src/main/java/ru/practicum/android/diploma/data/dto/responses/VacancyDetailResponse.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/responses/VacancyDetailResponse.kt
@@ -3,5 +3,18 @@ package ru.practicum.android.diploma.data.dto.responses
 import ru.practicum.android.diploma.data.dto.NetworkResponse
 
 data class VacancyDetailResponse(
-    val vacancy: VacancyDetailDTO? = null
+    val id: String?,
+    val name: String?,
+    val description: String?,
+    val salary: SalaryDTO?,
+    val address: AddressDTO?,
+    val experience: ExperienceDTO?,
+    val schedule: ScheduleDTO?,
+    val employment: EmploymentDTO?,
+    val contacts: ContactsDTO?,
+    val employer: EmployerDTO?,
+    val area: FilterAreaDTO?,
+    val skills: List<String>?,
+    val url: String?,
+    val industry: FilterIndustryDTO?,
 ) : NetworkResponse()
