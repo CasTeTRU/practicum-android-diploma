50a5c78d09e1be777a1bcd0627be733c584afad0
forevershagal <07ramilshagaliev@gmail.com>
2025-12-04 19:36:47 +0300

    feat: add to domain and util

---
+++ app/src/main/java/ru/practicum/android/diploma/favorites/domain/api/FavoriteVacancyRepository.kt
@@ -0,0 +1,5 @@
+package ru.practicum.android.diploma.favorites.domain.api
+
+interface FavoriteVacancyRepository {
+    // В будущем здесь будут методы для работы с БД
+}
---
+++ app/src/main/java/ru/practicum/android/diploma/presentation/vacancy/models/NavigationEventState.kt
@@ -0,0 +1,8 @@
+package ru.practicum.android.diploma.presentation.vacancy.models
+
+import android.content.Intent
+
+data class NavigationEventState(
+    val intent: Intent,
+    val errorMessage: String
+)
